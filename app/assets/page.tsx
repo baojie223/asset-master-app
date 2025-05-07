6daf7b0d5b1ab8f6db6b85243d05947f75366820
Jie Bao <baojiedashuaibi@gmail.com>
2025-05-07 06:46:35 +0000

    feat: Add supabase

--- app/assets/page.tsx
+++ app/assets/page.tsx
@@ -1,43 +1,61 @@
 "use client";
 
-import { AllCommunityModule, ColDef, ModuleRegistry } from "ag-grid-community";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
 ModuleRegistry.registerModules([AllCommunityModule]);
 
-import { AgGridReact } from "ag-grid-react";
-import { useState } from "react";
-import {
-  CartesianGrid,
-  Line,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
-import { LineChart } from "recharts";
+// import { AgGridReact } from "ag-grid-react";
+// import { useState } from "react";
+// import {
+//   CartesianGrid,
+//   Line,
+//   ResponsiveContainer,
+//   Tooltip,
+//   XAxis,
+//   YAxis,
+// } from "recharts";
+// import { LineChart } from "recharts";
+
+import { createClient } from "@supabase/supabase-js";
+import { useEffect } from "react";
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  "https://rzjeoeljpbdgxmlohlos.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6amVvZWxqcGJkZ3htbG9obG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1OTU0MjQsImV4cCI6MjA2MjE3MTQyNH0.tBBR7I880f7W0d1_ITe4tCjx4pkQYNmW29_mt4uBI-A"
+);
 
 export default function Assets() {
+  // const { data, error } = await supabase.from("asset").select();
   // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState<any[]>([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  ]);
+  // const [rowData, setRowData] = useState<any[]>([
+  //   { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+  //   { make: "Ford", model: "F-Series", price: 33850, electric: false },
+  //   { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  // ]);
 
-  // Column Definitions: Defines & controls grid columns.
-  const [colDefs, setColDefs] = useState<ColDef<any>[]>([
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" },
-  ]);
+  // // Column Definitions: Defines & controls grid columns.
+  // const [colDefs, setColDefs] = useState<ColDef<any>[]>([
+  //   { field: "make" },
+  //   { field: "model" },
+  //   { field: "price" },
+  //   { field: "electric" },
+  // ]);
 
-  const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
+  // const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
+  // console.log(data);
+
+  useEffect(() => {
+    async function getData() {
+      const { data, error } = await supabase.from("assets").select("*");
+      console.log(data, error);
+    }
+    getData();
+  }, []);
 
   return (
-    <div className="h-screen w-screen">
-      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+    <div className="h-screen w-screen bg-white">
+      {/* <AgGridReact rowData={rowData} columnDefs={colDefs} /> */}
 
-      <ResponsiveContainer width="100%" height="100%">
+      {/* <ResponsiveContainer width="100%" height="100%">
         <LineChart
           width={600}
           height={300}
@@ -49,7 +67,7 @@ export default function Assets() {
           <XAxis dataKey="name" />
           <YAxis />
         </LineChart>
-      </ResponsiveContainer>
+      </ResponsiveContainer> */}
     </div>
   );
 }
