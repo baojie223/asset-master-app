cd1e1dc3640129818186a0b4b1064c4b5ad073cc
Jie Bao <baojiedashuaibi@gmail.com>
2025-05-07 16:04:17 +0000

    feat: Test

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className=" font-[family-name:var(--font-geist-sans)]">
       <main className="">
-        <Link href="/assets">Assets</Link>
+        <Link href="/assets">Assets test1</Link>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
